037d41f70876e6a412821068a5bb8246e89c7065
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2018-08-29 19:54:07 +0300

    add tests for compute age function

---
+++ test_computeAge.py
@@ -0,0 +1,18 @@
+from computeAge import computeAge
+import unittest
+
+
+class Testtest(unittest.TestCase):
+
+    def test_computeAge(self):
+        self.assertEqual(computeAge(2015), 'you are a minor')
+        self.assertEqual(computeAge(2000), 'you are a youth')
+        self.assertEqual(computeAge(1970), 'you are an elder')
+        self.assertEqual(computeAge(2019), 'you can not have a negative age')
+          
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
