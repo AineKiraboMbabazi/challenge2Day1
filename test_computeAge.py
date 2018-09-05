9b59843ee7d1cb8b53893f18919a42dfbe9fdf23
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2018-09-05 21:45:37 +0300

    minor modifications to the test files

--- test_computeAge.py
+++ test_computeAge.py
@@ -9,10 +9,13 @@ class Testtest(unittest.TestCase):
         self.assertEqual(computeAge(2000), 'you are a youth')
         self.assertEqual(computeAge(1970), 'you are an elder')
         self.assertEqual(computeAge(2019), 'you can not have a negative age')
-          
+    @unittest.skip('work in progress')
+    def test_input_is_int(self):
+        self.assertRaises(computeAge('yob'),ValueError )   
 
 
 
 
 if __name__ == '__main__':
     unittest.main()
+    
